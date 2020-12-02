83b211dae724d0ea0eb43aa6e5e734299ccd476f
Lucas <lucasaxelson2016@gmail.com>
2020-12-02 22:28:09 +0000

    02.12.2020

---
+++ Test/main.js
@@ -0,0 +1,45 @@
+var count;
+for (count = 1; count <= 5; count++) {
+    alert(count);
+};
+
+/*
+var count = 0;
+while (count < 5) {
+    console.log(count);
+    count = count + 1;
+    //    count++;
+};
+
+if (count = 5) {
+    alert('Count has exceeded it\'s maxiumum');
+}
+
+var age = prompt('What is your age?')
+if (age < 18) {
+    alert('Under age')
+}
+else {
+    alert('Welcome!')
+}
+
+
+var class1 = [{ name: "Lucas", age: 23, sex: "Male" }, { name: "Gabriela", age: 19, sex: "Female" }]
+
+console.log(class1);
+alert(class1[1].name);
+
+
+
+var list = ["Lucas", "Chris", "Raphael"];
+list.push("Gabriela")
+
+alert(list.join(" / "));
+alert(list);
+
+
+var lucas = { name: "Lucas", idade: 23, sex: "Male" };
+
+alert(lucas.name);
+alert(lucas.sex);
+*/
