45ec34292012cf1cd21a39a0337565a07109e619
Lucas <lucasaxelson2016@gmail.com>
2021-01-05 00:13:09 +0000

    04/01/2021

---
+++ Test/function/callbacks/main.js
@@ -0,0 +1,15 @@
+const greet = (str) => {
+    return `Hey ${str}!`
+};
+
+const greet = (str) => { return console.log(`Hey ${str}!`) };
+
+// const example = ["   timothee", "  darth-hater"]
+
+// function cleanNames(strArray) {
+//     let answer = strArray.map(function (str) {
+//         return str.trim();
+//     })
+//     return answer
+// }
+
---
+++ math/main.js
@@ -0,0 +1,7 @@
+const myMath = {
+    add(x, y) { return x + y },
+    multiply(x, y) { return x * y },
+    divide(x, y) { return x / y },
+    square(x) { return x ** 2 },
+    cube(x) { return x ** 3 }
+}
