d06c11c465e0688127ddcc37023b57abae4ecb3e
Lucas <lucasaxelson2016@gmail.com>
2021-01-06 20:17:19 +0000

    06/01/2021

--- Test/function/callbacks/main.js
+++ Test/function/callbacks/main.js
@@ -1,7 +1,18 @@
+function allEvens(arrayNum) {
+    let evens = arrayNum.every(num => {
+        return num % 2 === 0;
+    });
+    return evens;
+};
+
+const allOdd = ((arrayNum) => {
+    return arrayNum.every(num => { return num % 2 !== 0 });
+})
+
 
 // const validUserNames = .filter(name => name.length < 10);
 
-const validUserNames = (array) => (array.filter(name => name.length < 10));
+// const validUserNames = (array) => (array.filter(name => name.length < 10));
 
 // const greet = (str) => {
 //     return `Hey ${str}!`
