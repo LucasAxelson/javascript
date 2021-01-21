4169e74e7cf9363dbf2dba46ba3d7951f9ba9081
Lucas <lucasaxelson2016@gmail.com>
2021-01-21 16:53:49 +0000

    Style

--- events/style/app.js
+++ events/style/app.js
@@ -2,24 +2,44 @@ const randomColor = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
-
     return `rgb(${r}, ${g}, ${b})`;
+}
+
+function colorize() {
+    this.style.backgroundColor = randomColor();
+    this.style.color = randomColor();
 }
 
 const container = document.getElementById('container');
 
 for (let i = 0; i < 20; i++) {
-    const node = document.createElement('button');
     const box = document.createElement('div');
+    const node = document.createElement('button');
+    const phrase = document.createElement('span');
     node.innerHTML = '<b>Hey!</b>';
-    container.appendChild(box);
+    phrase.innerHTML = 'Welcome to a Colourful Life!';
     box.appendChild(node);
-}
+    box.appendChild(phrase);
+    container.appendChild(box);
+};
 
 const buttons = document.querySelectorAll('button');
 
 for (let button of buttons) {
-    button.addEventListener('click', function () {
-        button.style.backgroundColor = randomColor();
-    })
-}
+    button.addEventListener('click', colorize)
+};
+
+const spans = document.querySelectorAll('span');
+
+for (let span of spans) {
+    span.addEventListener('click', colorize)
+};
+
+// const buttons = document.querySelectorAll('button');
+
+// for (let button of buttons) {
+//     button.addEventListener('click', function () {
+//         button.style.backgroundColor = randomColor();
+//     })
+// }
+
