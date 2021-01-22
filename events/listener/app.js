7c452c0c39106cca460326110703a39100a45016
Lucas <lucasaxelson2016@gmail.com>
2021-01-22 23:17:43 +0000

    Listener Update

--- events/listener/app.js
+++ events/listener/app.js
@@ -28,14 +28,3 @@ form.addEventListener('submit', function (e) {
 //     item.insertAdjacentText('beforeend', qty);
 //     ul.insertAdjacentElement('afterbegin', item);
 // };
-
-// const hello = document.querySelector('#hello');
-// const goodbye = document.querySelector('#goodbye');
-
-// hello.addEventListener('click', function () {
-//     console.log('hello');
-// });
-
-// goodbye.addEventListener('click', function () {
-//     console.log('goodbye');
-// });
