12d1b7e6ef0055b8f98d039e425acf746a0991d1
Lucas Axelson <lucasaxelson2016@gmail.com>
2021-02-05 12:10:20 +0000

    05/02/2021

--- score-keeper/app.js
+++ score-keeper/app.js
@@ -1,22 +1,41 @@
 const p1Button = document.querySelector('#p1Button')
-const p2Button = document.querySelector('#p2Button')
-const p1Display = document.querySelector('#p1Display')
-const p2Display = document.querySelector('#p2Display')
+const p2Button = document.getElementById('p2Button')
+const resetButton = document.getElementById('reset')
+
+const p1Display = document.getElementById('p1Display')
+const p2Display = document.getElementById('p2Display')
+const scoreSelect = document.getElementById('playto')
 
 let p1Score = 0
 let p2Score = 0
 let winningScore = 5
 let isGameOver = false
 
-p1Button.addEventListener('click',function(){
-    if (p1Score !== winningScore) {
+p1Button.addEventListener('click', () => {
+    if (!isGameOver) {
     p1Score += 1
+    if (p1Score === winningScore) {
     p1Display.textContent = p1Score
-}
+        } 
+    }
 })
-p2Button.addEventListener('click',function(){
-    if (p2Score !== winningScore) {
-    p2Score += 1
-    p2Display.textContent = p2Score
-}
+p2Button.addEventListener('click', () => {
+    if (!isGameOver) {
+        p2Score += 1
+        if (p2Score === winningScore) {
+        p2Display.textContent = p2Score
+            } 
+        }
+})
+
+scoreSelect.addEventListener('change', () => {
+    winningScore
+})
+
+resetButton.addEventListener('click', () => {
+    isGameOver = false
+    p1Score = 0
+    p2Score = 0
+    p1Display.textContent = 0 
+    p2Display.textContent = 0 
 })
