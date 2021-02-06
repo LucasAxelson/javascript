ff2c837821c9405d7fef09462ff0d5cfa21baf3f
Lucas <lucasaxelson2016@gmail.com>
2021-02-06 14:41:25 +0000

    Score Keeper Final

--- score-keeper/app.js
+++ score-keeper/app.js
@@ -1,51 +1,55 @@
-const p1Button = document.querySelector('#p1Button')
-const p2Button = document.getElementById('p2Button')
+const P1 = {
+    score: 0,
+    button: document.getElementById('p1Button'),
+    display: document.getElementById('p1Display'),
+}
+
+const P2 = {
+    score: 0,
+    button: document.getElementById('p2Button'),
+    display: document.getElementById('p2Display'),
+}
+
 const resetButton = document.getElementById('reset')
-
-const p1Display = document.getElementById('p1Display')
-const p2Display = document.getElementById('p2Display')
-const scoreSelect = document.getElementById('playto')
-
-let p1Score = 0
-let p2Score = 0
-let winningScore = 5
+const winningScoreSelect = document.getElementById('playto')
+let winningScore = 3
 let isGameOver = false
 
-p1Button.addEventListener('click', () => {
+function updateScores(player, opponent) {
     if (!isGameOver) {
-        p1Score += 1
-        if (p1Score === winningScore) {
+        player.score += 1
+        if (player.score === winningScore) {
             isGameOver = true
-            p1Display.classList.add('Winner')
-            p2Display.classList.add('Loser')
+            player.display.classList.add('has-text-success')
+            opponent.display.classList.add('has-text-danger')
+            player.button.disabled = true
+            opponent.button.disabled = true
         }
-        p1Display.textContent = p1Score
+        player.display.textContent = player.score
     }
-})
-p2Button.addEventListener('click', () => {
-    if (!isGameOver) {
-        p2Score += 1
-        if (p2Score === winningScore) {
-            isGameOver = true
-            p2Display.classList.add('Winner')
-            p1Display.classList.add('Loser')
-        }
-        p2Display.textContent = p2Score
-    }
+}
+
+P1.button.addEventListener('click', () => {
+    updateScores(P1, P2)
 })
 
-scoreSelect.addEventListener('change', () => {
+P2.button.addEventListener('click', () => {
+    updateScores(P2, P1)
+})
+
+winningScoreSelect.addEventListener('change', () => {
     winningScore = parseInt(this.value)
     reset()
 })
 
-resetButton.addEventListener('click', () => {
-    isGameOver = false
-    p1Score = 0
-    p2Score = 0
-    p1Display.textContent = 0
-    p2Display.textContent = 0
-    p1Display.classList.remove('Winner', 'Loser')
-    p1Display.classList.remove('Winner', 'Loser')
+resetButton.addEventListener('click', reset)
 
-})
+function reset() {
+    isGameOver = false
+    for (let P of [P1, P2]) {
+        P.score = 0
+        P.display.textContent = 0
+        P.display.classList.remove('has-text-success', 'has-text-danger')
+        P.button.disabled = false
+    }
+}
