a5f25e246abf649bd1002cb31f5e6de73bc239d3
Lucas <lucasaxelson2016@gmail.com>
2021-02-07 23:53:17 +0000

    Promise File

--- async/promises/app.js
+++ async/promises/app.js
@@ -1,19 +1,36 @@
-const fakeRequest = (url) => {
+const delayedColorChange = (color, delay) => {
     return new Promise((resolve, reject) => {
-        const rand = Math.random()
         setTimeout(() => {
-            if (rand < 0.7) {
-                resolve('Your fake data:')
-            } reject('Request Error')
-        }, 1000)
+            document.body.style.backgroundColor = color
+            resolve()
+        }, delay)
     })
 }
 
-fakeRequest('/dogs/1')
-    .then((data) => {
-        console.log('Request Successful')
-        console.log(data)
-    })
-    .catch((err) => {
-        console.log('Failure.', err)
-    })
+delayedColorChange('red', 1000)
+    .then(() => delayedColorChange('blue', 2000))
+    .then(() => delayedColorChange('green', 3000))
+    .then(() => delayedColorChange('purple', 2000))
+    .then(() => delayedColorChange('violet', 1000))
+    .then(() => delayedColorChange('yellow', 2000))
+    .then(() => delayedColorChange('orange', 3000))
+
+// const fakeRequest = (url) => {
+//     return new Promise((resolve, reject) => {
+//         const rand = Math.random()
+//         setTimeout(() => {
+//             if (rand < 0.7) {
+//                 resolve('Your fake data:')
+//             } reject('Request Error')
+//         }, 1000)
+//     })
+// }
+
+// fakeRequest('/dogs/1')
+//     .then((data) => {
+//         console.log('Request Successful')
+//         console.log(data)
+//     })
+//     .catch((err) => {
+//         console.log('Failure.', err)
+//     })
