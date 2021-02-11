acc4d44ea8ece25290a4053c2006ae1c081892b9
Lucas <lucasaxelson2016@gmail.com>
2021-02-11 08:44:43 +0000

    Axios

---
+++ api-async/axios/app.js
@@ -0,0 +1,16 @@
+axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+    .then(res => {
+        console.log(res.data.ticker.price)
+    })
+    .catch(e => {
+        console.log('Error', e)
+    })
+
+const fetchBitPrice = async () => {
+    try {
+        const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+        console.log(res.data.ticker.price)
+    } catch (e) {
+        console.log('Error caught', e)
+    }
+}
