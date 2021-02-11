bde268dd58893f418aa58d60fa91c016b9ddbb8d
Lucas <lucasaxelson2016@gmail.com>
2021-02-11 13:58:30 +0000

    Axios Practice

--- api-async/axios/app.js
+++ api-async/axios/app.js
@@ -1,16 +1,37 @@
-axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
-    .then(res => {
-        console.log(res.data.ticker.price)
-    })
-    .catch(e => {
-        console.log('Error', e)
-    })
-
-const fetchBitPrice = async () => {
+const getDadJoke = async () => {
     try {
-        const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
-        console.log(res.data.ticker.price)
+        const config = { headers: { Accept: 'application/json', } }
+        const res = await axios.get('https://icanhazdadjoke.com/', config)
+        return res.data.joke
     } catch (e) {
-        console.log('Error caught', e)
+        return "No Joke found. Try again."
     }
 }
+
+const jokes = document.getElementById('jokes')
+const addDadJoke = async () => {
+    const joke = await getDadJoke()
+    const newLi = document.createElement('li')
+    newLi.append(joke)
+    jokes.append(newLi)
+}
+
+const button = document.querySelector('button')
+button.addEventListener('click', addDadJoke)
+
+// axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+//     .then(res => {
+//         console.log(res.data.ticker.price)
+//     })
+//     .catch(e => {/
+//         console.log('Error', e)
+//     })
+
+// const fetchBitPrice = async () => {
+//     try {
+//         const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+//         console.log(res.data.ticker.price)
+//     } catch (e) {
+//         console.log('Error caught', e)
+//     }
+// }
