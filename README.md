822fed9950b165f4cf20802d11c2a03de516d4c1
Lucas Axelson <lucasaxelson2016@gmail.com>
2021-02-15 12:51:53 +0000

    Readjustments

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ Mini-Projects based on Colt Steele's Web Developer 2021 Course on Udemy and addi
 
 There are not that many comments or hints, since these are my student notes and more an archive of solutions and ideas accumulated into one large repository as to avoid a cluster. <br>
 
+Note that some files may not run in server, this is due to the necessity to reorganize the files. Consequentely, some index.html files are not connected with their sister app.js. Correcting that, they should function normally. <br>
+
 if you have any doubts or questions, I am free to answer.
--- basic/events/listener/app.js
+++ basic/events/listener/app.js
@@ -28,3 +28,26 @@ form.addEventListener('submit', function (e) {
 //     item.insertAdjacentText('beforeend', qty);
 //     ul.insertAdjacentElement('afterbegin', item);
 // };
+
+// const input = document.querySelector('#username');
+// let h1 = document.querySelector('h1');
+
+// input.addEventListener('input', function () {
+//     hValue = username.value;
+//     h1.innerHTML = `Welcome, ` + hValue;
+//     if (hValue.length === 0) {
+//         h1.innerText = 'Enter Your Username';
+//     }
+// });
+
+
+// const hello = document.querySelector('#hello');
+// const goodbye = document.querySelector('#goodbye');
+
+// hello.addEventListener('click', function () {
+//     console.log('hello');
+// });
+
+// goodbye.addEventListener('click', function () {
+//     console.log('goodbye');
+// });
