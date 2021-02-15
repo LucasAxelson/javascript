af9ccfe7df14c1d0f8bec99a4ccfcc18ec332de9
Lucas Axelson <lucasaxelson2016@gmail.com>
2021-02-15 12:15:52 +0000

    Modern Classes

--- intermediate/constructor/app.js
+++ intermediate/constructor/app.js
@@ -1,24 +1,51 @@
-function color(r, g, b) {
-    this.r = r
-    this.g = g
-    this.b = b
+// function color(r, g, b) {
+//     this.r = r
+//     this.g = g
+//     this.b = b
+// }
+
+// Color.prototype.rgb = function () {
+//     const { r, g, b } = this
+//     return `rgb(${r}, ${g}, ${b})`
+// }
+
+// Color.prototype.hex = function () {
+//     const { r, g, b } = this
+//     return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+// }
+
+// Color.prototype.rgba = function (a = 1.0) {
+//     const { r, g, b } = this
+//     return `rgba(${r}, ${g}, ${b}, ${a})`
+// }
+
+
+// const color1 = new Color(29, 244, 64)
+// const color2 = new Color(12, 134, 33)
+
+class Color {
+    constructor(r, g, b, name) {
+        this.r = r
+        this.b = b
+        this.g = g
+        this.name = name
+    }
+    innerRGB() {
+        const { r, g, b } = this
+        return `${r}, ${g}, ${b}`
+
+    }
+    rgb() {
+        return `rgb(${this.innerRGB()})`
+    }
+    rgba(a = 1.0) {
+        return `rgba(${this.innerRGB()}, ${a})`
+    }
+    hex() {
+        const { r, g, b } = this
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+    }
 }
 
-Color.prototype.rgb = function () {
-    const { r, g, b } = this
-    return `rgb(${r}, ${g}, ${b})`
-}
-
-Color.prototype.hex = function () {
-    const { r, g, b } = this
-    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
-}
-
-Color.prototype.rgba = function (a = 1.0) {
-    const { r, g, b } = this
-    return `rgba(${r}, ${g}, ${b}, ${a})`
-}
-
-
-const color1 = new Color(29, 244, 64)
-const color2 = new Color(12, 134, 33)
+const black = new Color(0, 0, 0, 'black')
+const white = new Color(255, 255, 255, 'white')
