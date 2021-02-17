62051cdf9392e3804c6cd0a5bc1815e2276c4ab8
Lucas Axelson <lucasaxelson2016@gmail.com>
2021-02-17 01:07:21 +0000

    Aux. Lessons - 17-02

---
+++ starter-studies/refinements/fundamentals/app.js
@@ -0,0 +1,31 @@
+// Ternary Operator -> (Expression) ? (Statement) : (Sec. Statement)
+// If exp. is true, execute first stat. If not, execute sec. stat.
+// ====================== *** ==================
+// Expressions = Actions -> Execution of a statement
+// 23 + 25 = 48 -> Expression
+// let variable = 23 + 25 = 48 -> Statement 
+
+// ====================== *** ==================
+// SWITCH Statements
+
+// let day = 'monday'
+
+// switch (day) {
+//     case 'monday':
+//         console.log(`Monday`)
+//         break
+//     case 'tuesday':
+//         console.log(`Tuesday`)
+//         break
+//     case 'wednesday':
+//         console.log(`Wednesday`)
+//         break
+//     case 'thursday':
+//         console.log(`Thursday`)
+//         break
+//     case 'friday':
+//         console.log(`Friday`)
+//         break
+//     default:
+//         console.log(`Must be the weekend`)
+// }
