4ea355dc5895471fbc1abfe13062a8e86db80086
Lucas Axelson <lucasaxelson2016@GMAIL.COM>
2021-02-19 00:51:34 +0000

    Code Challenge #4

--- code-challenges/fundamentals/app.js
+++ code-challenges/fundamentals/app.js
@@ -1,30 +1,42 @@
+// $15 tip, if purchase is between 50 & 300
+// $20 tip, if purchase is different.
+// Test Data: £275 -- $40 -- $430
+
+function tipBill(perc, bill) {
+    let tip = (perc / 100) * bill
+    console.log(`Your bill is £${bill}, and the tip will be £${tip}`)
+}
+
+let bill = 430
+bill >= 50 && bill <= 300 ? tipBill(15, bill) : tipBill(20, bill)
+
+// =================== **** =======================
 // Data 1: Dolphins: 96, 108 and 89. Koalas: 88, 91 and 110
 // Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
 // Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
 
+// const dolphinsScore = [7, 112, 101]
+// const koalasScore = [19, 95, 106]
+// const reducer = (accumulator, currentValue) => accumulator + currentValue
 
-const dolphinsScore = [7, 112, 101]
-const koalasScore = [19, 95, 106]
-const reducer = (accumulator, currentValue) => accumulator + currentValue
+// function calculateScore(array1, array2) {
+//     let score1 = array1.reduce(reducer)
+//     score1 = score1 / 3
+//     console.log(score1)
 
-function calculateScore(array1, array2) {
-    let score1 = array1.reduce(reducer)
-    score1 = score1 / 3
-    console.log(score1)
+//     let score2 = array2.reduce(reducer)
+//     score2 = score2 / 3
+//     console.log(score2)
 
-    let score2 = array2.reduce(reducer)
-    score2 = score2 / 3
-    console.log(score2)
+//     return determineWinner(score1, score2)
+// }
 
-    return determineWinner(score1, score2)
-}
-
-function determineWinner(score1, score2) {
-    if (score1 > score2 && score1 >= 100) return `Congratulations, Dolphins! You are the winners`;
-    else if (score2 > score1 && score2 >= 100) return `Congratulations, Koalas! You are the winners`;
-    else if (score1 == score2 && score1 > 100) return `No winners today! It's a draw!!`;
-    else return `Neither teams have surpassed the minimum score of 100. Both teams lose.`;
-}
+// function determineWinner(score1, score2) {
+//     if (score1 > score2 && score1 >= 100) return `Congratulations, Dolphins! You are the winners`;
+//     else if (score2 > score1 && score2 >= 100) return `Congratulations, Koalas! You are the winners`;
+//     else if (score1 == score2 && score1 > 100) return `No winners today! It's a draw!!`;
+//     else return `Neither teams have surpassed the minimum score of 100. Both teams lose.`;
+// }
 
 // =================== **** =======================
 // // Mass in Kilograms (KG) -- Height is Meters (M)
--- starter-studies/refinements/fundamentals/app.js
+++ starter-studies/refinements/fundamentals/app.js
@@ -1,5 +1,15 @@
 // Ternary Operator -> (Expression) ? (Statement) : (Sec. Statement)
 // If exp. is true, execute first stat. If not, execute sec. stat.
+
+// let gender = 'male'
+// gender = 'male' ? console.log('Male') : console.log('Woman')
+
+// const randomNum = () => Math.floor(Math.random() * 10) + 1
+// randomNum() > 5 ? console.log(`Number above 5.`) : console.log(`Number below 5.`)
+
+// if (randomNum > 3) randomNum()
+// else console.log('3 or Below')
+
 // ====================== *** ==================
 // Expressions = Actions -> Execution of a statement
 // 23 + 25 = 48 -> Expression
