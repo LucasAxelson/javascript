a932dd4a39a8d1a709760b15f3168c3d6ac6e37d
Lucas Axelson <lucasaxelson2016@gmail.com>
2021-03-23 11:52:23 +0000

    Loops

--- studies/refinements/fundamentals/app.js
+++ studies/refinements/fundamentals/app.js
@@ -1,3 +1,44 @@
+// Continue & Breaks
+const mixArray = [`john`, `jay`, true, 13, 14, `liam`];
+const strArray = [];
+
+for (let i = 0; i < mixArray.length; i++) {
+  if (typeof mixArray[i] !== `string`) continue;
+  strArray[i] = mixArray[i];
+}
+console.log(strArray);
+
+// Break with numbers
+for (let i = 0; i < mixArray.length; i++) {
+  if (typeof mixArray[i] === `number`) break;
+  console.log(mixArray[i], typeof mixArray[i]);
+  //   Log no other elements once a number is found
+}
+
+// Creation of an array from within an array
+const testArray = [`john`, 1, true, [`a`, `b`, `c`]];
+const typeOf = []; // First declare outside of primary array
+
+for (let i = 0; i < testArray.length; i++) {
+  //   typeOf[i] = typeof testArray[i];
+  typeOf.push(typeof testArray[i]);
+}
+
+console.log(typeOf);
+
+// Second example
+
+const years = [1991, 2007, 1969, 2018];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  ages[i] = 2021 - years[i];
+  // ages.push()
+}
+
+console.log(ages);
+
+// ====================== *** ==================
 // Ternary Operator -> (Expression) ? (Statement) : (Sec. Statement)
 // If exp. is true, execute first stat. If not, execute sec. stat.
 
@@ -13,7 +54,7 @@
 // ====================== *** ==================
 // Expressions = Actions -> Execution of a statement
 // 23 + 25 = 48 -> Expression
-// let variable = 23 + 25 = 48 -> Statement 
+// let variable = 23 + 25 = 48 -> Statement
 
 // ====================== *** ==================
 // SWITCH Statements
