518d86bb62d762149fe260735568c67c2c68039f
Lucas Axelson <lucasaxelson2016@gmail.com>
2021-03-27 08:37:38 +0000

    Coding Challenge 2 & 3 - JS Fundamentals 2

--- code-challenges/fundamentals2/app.js
+++ code-challenges/fundamentals2/app.js
@@ -1,6 +1,74 @@
+// Coding Challenge 4
+
+// Coding Challenge 3
+
+// const Mark = {
+//   fullName: `Mark Miller`,
+//   height: 1.69,
+//   mass: 78,
+
+//   calcBMI() {
+//     bmi = this.mass / this.height ** 2;
+//     return bmi.toFixed(2);
+//   },
+// };
+
+// const John = {
+//   fullName: `John Smith`,
+//   height: 1.95,
+//   mass: 92,
+
+//   calcBMI() {
+//     let bmi = this.mass / this.height ** 2;
+//     return bmi.toFixed(2);
+//   },
+// };
+
+// if (John.calcBMI() > Mark.calcBMI()) {
+//   console.log(
+//     `${John.fullName}'s BMI (${John.calcBMI()}) is higher than ${
+//       Mark.fullName
+//     }'s (${Mark.calcBMI()})`
+//   );
+// } else {
+//   console.log(
+//     `${Mark.fullName}'s BMI (${Mark.calcBMI()}) is higher than ${
+//       John.fullName
+//     }'s (${John.calcBMI()})`
+//   );
+// }
+
 // Coding Challenge 2
 
-calcTip = (tip) => {};
+// const bills = [125, 555, 44];
+// const tips = [];
+// const bonus = [];
+
+// calcTip = (bill) => {
+//   let tip;
+//   if (bill >= 50 && bill <= 300) {
+//     tip = 0.15 * bill;
+//   } else {
+//     tip = 0.2 * bill;
+//   }
+//   return tip;
+// };
+
+// bills.forEach((bill) => {
+//   let tip = calcTip(bill);
+//   tips.push(tip);
+// });
+
+// for (let i = 0; i < bills.length; i++) {
+//   let result = bills[i] + tips[i];
+//   bonus.push(result);
+// }
+
+// console.log(
+//   `Bills: ${bills}
+// Tips: ${tips}
+// Bonuses: ${bonus}`
+// );
 
 // Coding Challenge 1
 // Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
