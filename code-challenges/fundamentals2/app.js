39a245c102b3db2ddfb42be02570d87e5651cb5b
Lucas Axelson <lucasaxelson2016@gmail.com>
2021-03-27 08:54:44 +0000

    Coding Challenge 4

--- code-challenges/fundamentals2/app.js
+++ code-challenges/fundamentals2/app.js
@@ -1,6 +1,34 @@
 // Coding Challenge 4
+// Calculate tips with loops and calculate total`s average
+let sum = 0;
+calcAverage = (arr) => {
+  for (let i = 0; i < bills.length; i++) {
+    sum += arr[i];
+  }
+  return sum / bills.length;
+};
+
+calcTip = (bill) => {
+  let tip;
+  if (bill >= 50 && bill <= 300) {
+    tip = 0.15 * bill;
+  } else {
+    tip = 0.2 * bill;
+  }
+  return tip;
+};
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = calcAverage(bills);
+
+bills.forEach((bill) => {
+  let tip = calcTip(bill);
+  tips.push(tip);
+});
 
 // Coding Challenge 3
+// Calculate the BMI of both people utilizing objects
 
 // const Mark = {
 //   fullName: `Mark Miller`,
@@ -39,10 +67,11 @@
 // }
 
 // Coding Challenge 2
+// Calculate the tips and total via arrays and loops
 
 // const bills = [125, 555, 44];
 // const tips = [];
-// const bonus = [];
+// const total = [];
 
 // calcTip = (bill) => {
 //   let tip;
@@ -61,16 +90,18 @@
 
 // for (let i = 0; i < bills.length; i++) {
 //   let result = bills[i] + tips[i];
-//   bonus.push(result);
+//   total.push(result);
 // }
 
 // console.log(
 //   `Bills: ${bills}
 // Tips: ${tips}
-// Bonuses: ${bonus}`
+// Total: ${total}`
 // );
 
 // Coding Challenge 1
+// Calculate the average score of the team to determine winner
+
 // Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
 // Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
 
