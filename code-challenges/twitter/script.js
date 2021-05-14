ee7461dd0a868af84cd3a691ff55ee33af971353
LucasAxelson <lucasaxelson2016@gmail.com>
2021-05-14 15:50:47 +0100

    Twitter-charCount | in progress

---
+++ code-challenges/twitter/script.js
@@ -0,0 +1,54 @@
+// VARIABLES
+const form = document.querySelector(`form`)
+const inputTweet = document.querySelector(`.input--tweet`)
+const btnTweet = document.querySelector(`.button--tweet`)
+const containerTweets = document.querySelector(`.container--tweets`)
+
+// FUNCTIONS
+function addTweet (input) {
+    const paragraph = document.createElement(`p`)
+    paragraph.innerText = input.value
+    containerTweets.append(paragraph)
+}
+
+function renderAlert(charCount) {
+    const alert = document.createElement(`p`)
+    const containerAlert = document.createElement(`div`)
+    containerAlert.classList.add(`container--alert`)
+
+    alert.innerText = `You have used too many characters (${charCount}). The limit is 280 characters.`
+    containerAlert.append(alert)
+
+    form.insertAdjacentElement(`beforebegin`, containerAlert)
+}
+
+function removeAlert() {
+    if (document.querySelector(`.container--alert`)) {
+    const containerAlert = document.querySelector(`.container--alert`)
+    containerAlert.remove()
+    }
+}
+
+function correctAlert() {
+    
+}
+
+
+// APP
+form.addEventListener(`submit`, (e) => {
+    e.preventDefault()
+    const charCount = inputTweet.value.length
+
+    if (charCount <= 10) {
+        addTweet(inputTweet)
+    }
+
+    if (charCount > 10 && !document.querySelector(`.container--alert`)) {
+        renderAlert(charCount)
+    } else if (charCount > 10 && document.querySelector(`.container--alert`)) {
+        renderAlert(charCount)
+    } else {
+        removeAlert()
+    }
+})
+
