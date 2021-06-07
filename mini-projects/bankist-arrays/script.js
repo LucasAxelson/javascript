9340aa318c7853bf360c0a5b5b7b92367e598bca
LucasAxelson <lucasaxelson2016@gmail.com>
2021-06-07 23:17:17 +0100

    folder specification

--- mini-projects/bankist-arrays/script.js
+++ mini-projects/bankist-arrays/script.js
@@ -61,6 +61,24 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function(movements) {
+  containerMovements.innerHTML = ``
+
+  movements.forEach((mov, i) => {
+    const type = mov > 0 ? `deposit` : `withdrawal`
+
+    const html = `
+    <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+    <div class="movements__date">3 days ago</div>
+    <div class="movements__value">£${mov}</div>
+  </div>
+`
+    containerMovements.insertAdjacentElement(`afterbegin` ,html)
+  })
+}
+
+displayMovements(account1.movements)
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -93,5 +111,10 @@ currenciesUnique.forEach(function(value, key, map) {
 /////////////////////////////////////////////////
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const movementsDescription = movements.map((mov, i) => {
+  `Movement ${i + 1}: You ${mov > 0 ? `deposited` : `withdrew`} ${Math.abs(mov)}`
+  }
+)
 
 /////////////////////////////////////////////////
--- studies/basic 2/dom/attributes-dom/main.js
+++ studies/basic 2/dom/attributes-dom/main.js
@@ -1,7 +1,16 @@
+// const container = document.getElementById('container');
 
+// for (let i = 0; i < 100; i++) {
+//     const node = document.createElement('button');
+//     node.innerHTML = '<b>Hey!</b>';
+//     container.appendChild(node);
+// }
 
+// const node = document.createElement('button');
+// node.innerHTML = '<b>Hey!</b>';
+// container.appendChild(node);
 
-// **Failed test 15/01/21
+// ==============================================
 
 function alterImg() {
     if (mainKey === myLock1) {
